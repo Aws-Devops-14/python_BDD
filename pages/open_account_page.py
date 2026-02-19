8238378b3fd4dea0e1e986b0985f556f19c0ce1d
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-19 23:49:30 +0530

    Update open_account_page.py

--- pages/open_account_page.py
+++ pages/open_account_page.py
@@ -1,16 +1,25 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
 
-def open_account(self):
-    self.click(self.OPEN_ACCOUNT_LINK)
 
-    # Wait until Open Account page loads
-    self.wait.until(EC.url_contains("openaccount.htm"))
+class OpenAccountPage(BasePage):
 
-    self.click(self.OPEN_BTN)
+    OPEN_ACCOUNT_LINK = (By.LINK_TEXT, "Open New Account")
+    OPEN_BTN = (By.XPATH, "//input[@value='Open New Account']")
+    ACCOUNT_ID = (By.ID, "newAccountId")
 
-    print("Waiting for account number...")
+    def open_account(self):
+        self.click(self.OPEN_ACCOUNT_LINK)
 
-    # Wait until account number is present
-    return self.wait.until(
-        EC.presence_of_element_located(self.ACCOUNT_ID)
-    ).text
+        # Wait until Open Account page loads
+        self.wait.until(EC.url_contains("openaccount.htm"))
+
+        self.click(self.OPEN_BTN)
+
+        print("Waiting for account number...")
+
+        # Wait until account number is present
+        return self.wait.until(
+            EC.presence_of_element_located(self.ACCOUNT_ID)
+        ).text
