5a020e6687d86dadeba61c5ae5fa89d4abab143a
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-20 00:15:38 +0530

    Update transfer_page.py

--- pages/transfer_page.py
+++ pages/transfer_page.py
@@ -14,37 +14,38 @@ class TransferPage(BasePage):
     SUCCESS_TEXT = (By.XPATH, "//h1[contains(text(),'Transfer Complete')]")
 
     def transfer_funds(self, amount):
-
+    
         self.click(self.TRANSFER_LINK)
-
+    
         self.wait.until(EC.visibility_of_element_located(self.AMOUNT))
-
+    
         self.send_keys(self.AMOUNT, amount)
-
-        # Wait until dropdowns have options
-        from_element = self.wait.until(
-            EC.presence_of_element_located(self.FROM_ACCOUNT)
+    
+        # Wait until dropdowns are populated
+        self.wait.until(lambda driver:
+            len(Select(driver.find_element(*self.FROM_ACCOUNT)).options) > 0
         )
-        to_element = self.wait.until(
-            EC.presence_of_element_located(self.TO_ACCOUNT)
+    
+        self.wait.until(lambda driver:
+            len(Select(driver.find_element(*self.TO_ACCOUNT)).options) > 0
         )
-
-        from_dropdown = Select(from_element)
-        to_dropdown = Select(to_element)
-
-        # Ensure options exist
-        if len(from_dropdown.options) > 0:
-            from_dropdown.select_by_index(0)
-
-        if len(to_dropdown.options) > 1:
-            to_dropdown.select_by_index(1)
-        else:
-            to_dropdown.select_by_index(0)
-
+    
+        from_dropdown = Select(self.driver.find_element(*self.FROM_ACCOUNT))
+        to_dropdown = Select(self.driver.find_element(*self.TO_ACCOUNT))
+    
+        from_accounts = [opt.get_attribute("value") for opt in from_dropdown.options]
+        to_accounts = [opt.get_attribute("value") for opt in to_dropdown.options]
+    
+        print("From accounts:", from_accounts)
+        print("To accounts:", to_accounts)
+    
+        from_dropdown.select_by_value(from_accounts[0])
+        to_dropdown.select_by_value(to_accounts[-1])
+    
         self.click(self.TRANSFER_BTN)
-
+    
         self.wait.until(
             EC.visibility_of_element_located(self.SUCCESS_TEXT)
         )
-
+    
         return self.get_text(self.SUCCESS_TEXT)
