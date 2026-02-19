cbdb0cd9c931394dc3079679e10199918cd3b9ae
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-19 23:57:40 +0530

    Update transfer_page.py

--- pages/transfer_page.py
+++ pages/transfer_page.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
+
 
 class TransferPage(BasePage):
 
@@ -9,7 +11,20 @@ class TransferPage(BasePage):
     SUCCESS_TEXT = (By.XPATH, "//h1[contains(text(),'Transfer Complete')]")
 
     def transfer_funds(self, amount):
+        # Click transfer funds link
         self.click(self.TRANSFER_LINK)
+
+        # Wait until amount field is visible
+        self.wait.until(EC.visibility_of_element_located(self.AMOUNT))
+
+        # Enter amount
         self.send_keys(self.AMOUNT, amount)
+
+        # Click transfer button
         self.click(self.TRANSFER_BTN)
+
+        # Wait until success message appears
+        self.wait.until(EC.visibility_of_element_located(self.SUCCESS_TEXT))
+
         return self.get_text(self.SUCCESS_TEXT)
+
