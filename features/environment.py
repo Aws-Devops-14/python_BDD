d3c81fe5c44ec65fc3852914342e4061c4b0ff6f
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-20 00:26:01 +0530

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,7 @@
 from utils.driver_factory import DriverFactory
 
 def before_all(context):
-    context.driver = DriverFactory.get_driver(browser="chrome", headless=False)
+    context.driver = DriverFactory.get_driver(browser="chrome", headless=True)
 
 def after_all(context):
     context.driver.quit()
