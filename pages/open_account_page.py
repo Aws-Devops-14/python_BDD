13e8cb911b09aecceb40f87659479080021053ec
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-20 00:15:03 +0530

    Update open_account_page.py

--- pages/open_account_page.py
+++ pages/open_account_page.py
@@ -8,18 +8,21 @@ class OpenAccountPage(BasePage):
     OPEN_ACCOUNT_LINK = (By.LINK_TEXT, "Open New Account")
     OPEN_BTN = (By.XPATH, "//input[@value='Open New Account']")
     ACCOUNT_ID = (By.ID, "newAccountId")
-
     def open_account(self):
         self.click(self.OPEN_ACCOUNT_LINK)
-
+    
         # Wait until Open Account page loads
         self.wait.until(EC.url_contains("openaccount.htm"))
-
+    
         self.click(self.OPEN_BTN)
-
+    
         print("Waiting for account number...")
-
-        # Wait until account number is present
-        return self.wait.until(
-            EC.presence_of_element_located(self.ACCOUNT_ID)
+    
+        # Wait until account number is visible (STRONG WAIT)
+        account_number = self.wait.until(
+            EC.visibility_of_element_located(self.ACCOUNT_ID)
         ).text
+    
+        print("New account created:", account_number)
+    
+        return account_number
