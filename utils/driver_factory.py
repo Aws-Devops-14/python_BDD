0f68ab986e5b5cd2203148e14744f31715b059aa
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-20 00:20:42 +0530

    Update driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -11,8 +11,7 @@ class DriverFactory:
 
             if headless:
                 options.add_argument("--headless")
-                
-            options.add_argument("--window-size=1920,1080")    
+                options.add_argument("--window-size=1920,1080") 
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
 
