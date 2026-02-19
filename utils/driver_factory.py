a62b3deba9620047870976f2246d7cb6023e04ee
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-19 23:04:34 +0530

    Update driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -4,19 +4,13 @@ from selenium.webdriver.chrome.options import Options
 class DriverFactory:
 
     @staticmethod
-    def get_driver(browser="chrome", headless=False):
+    def get_driver():
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        return driver
 
-        if browser.lower() == "chrome":
-            options = Options()
-
-            if headless:
-                options.add_argument("--headless")
-                options.add_argument("--disable-gpu")
-
-            options.add_argument("--start-maximized")
-
-            driver = webdriver.Chrome(options=options)
-            return driver
-
-        else:
-            raise Exception(f"Browser '{browser}' not supported yet")
+       
