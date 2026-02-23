6a138eaf49fd18e81149cca0427b39a71983a7e7
Aws-Devops-14 <palemusharani5@gmail.com>
2026-02-24 00:56:44 +0530

    changes

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 class DriverFactory:
 
     @staticmethod
-    def get_driver(browser="chrome", headless=True):
+    def get_driver(browser="chrome", headless=False):
 
         if browser == "chrome":
             options = Options()
